90609bd2d3ad45a4c4597ec7975fb79febbf4d9a
Paolo Jovon <paolo.jovon@gmail.com>
2018-10-05 22:04:04 +0100

    WIP: Started implementing unit move-to-mouse behaviour

---
+++ Money_Tree/Assets/RTS/Terrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    public string UnitTag = "PlayerUnit";
+
+	// Use this for initialization
+	void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	}
+
+    void OnMouseDown()
+    {
+        var clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);    
+        RaycastHit clickRayHit;
+        var clickHit = Physics.Raycast(clickRay, out clickRayHit);
+
+        var units = GameObject.FindGameObjectsWithTag(UnitTag);
+        foreach(GameObject unit in units)
+        {
+            unit.SendMessage("UnitMoveTowards", clickRayHit.point);
+        }
+    }
+}
--- Money_Tree/Assets/RTS/UnitMover.cs
+++ Money_Tree/Assets/RTS/UnitMover.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitMover : MonoBehaviour {
+public class UnitMover : MonoBehaviour 
+{
+    bool _selected = false;
+    Vector3 _targetPos = Vector3.zero;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		
 	}
+
+	void OnMouseDown()
+	{
+        // Clicking on an unit's collider [de]selects it
+        _selected = !_selected;
+	}
+
+
+    // Tell the unit to start moving towards a point
+    public void UnitMoveTowards(Vector3 point)
+    {
+        // TODO IMPLEMENT
+    }
 }
