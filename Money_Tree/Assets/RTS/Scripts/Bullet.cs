d456ba6a25c70a9c21bca52802d9a33d6a1be11f
Paolo Jovon <paolo.jovon@gmail.com>
2018-10-05 23:37:25 +0100

    Started implementing Health and Bullet components

---
+++ Money_Tree/Assets/RTS/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // How much damage the bullet will do when it collides
+    public uint Damage = 50;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var otherObject = collision.gameObject;
+        var otherHealth = otherObject.GetComponent<Health>();
+        if(otherHealth != null && this.tag != otherObject.tag)
+        {
+            // We hit an enemy that takes damage
+            otherHealth.TakeDamage(Damage);
+        }
+    }
+}
---
+++ Money_Tree/Assets/RTS/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // The hitpoints the entity can withstand.
+    public uint HP = 50;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+	
+    // Makes the entity take some damage; destroys it if HP < 0
+    public void TakeDamage(uint damage)
+    {
+        if(damage < HP)
+        {
+            HP -= damage;
+        }
+        else
+        {
+            // Entity took excessive damage and is now dying
+            // TODO: Trigger death animation
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
