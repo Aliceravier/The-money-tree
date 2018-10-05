1502479a947c2bdcdfb6f3c6c9569fca200033bf
Aliceravier <aliceravier@yahoo.co.uk>
2018-10-06 00:16:09 +0200

    Implement shoot but not actual shoot function, just helper functions

---
+++ Money_Tree/Assets/Positioning.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Positioning : MonoBehaviour {
+
+    GameObject[] enemies;
+
+
+	// Use this for initialization
+	void Start () {
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject findNearestEnemy()
+    {
+        float minDistance = distanceTo(enemies[0]);
+        GameObject nearestEnemy = enemies[0];
+        foreach(GameObject enemy in enemies)
+        {
+            if(distanceTo(enemy) < minDistance)
+            {
+                nearestEnemy = enemy;
+                minDistance = distanceTo(enemy);
+            }
+        }
+        return nearestEnemy;
+    }
+
+    public bool inRangeOfEnemy(GameObject enemy)
+    {
+        int range = this.gameObject.GetComponent<Shoot>().range;
+        return (distanceTo(enemy) < range);
+    }
+
+    public float distanceTo(GameObject thing)
+    {
+        float xa = this.transform.position.x;
+        float ya = this.transform.position.y;
+        float xb = thing.transform.position.x;       
+        float yb = thing.transform.position.y;
+        return Mathf.Sqrt(Mathf.Pow(xb-xa, 2) + Mathf.Pow(yb - ya, 2))
+    }
+}
---
+++ Money_Tree/Assets/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public int range;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject nearestEnemy = this.GetComponent<Positioning>().findNearestEnemy();
+        if (this.GetComponent<Positioning>().inRangeOfEnemy(nearestEnemy))
+        {
+            shoot(nearestEnemy);
+        }
+	}
+
+    public void shoot(GameObject enemy)
+    {
+
+    }
+}
