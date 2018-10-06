b22449baab2739a2f4d2fe0c7deb2d53a0d2ff04
Paolo Jovon <paolo.jovon@gmail.com>
2018-10-06 19:07:48 +0100

    WIP: Extracted selection from UnitMover to its own component type

--- Money_Tree/Assets/RTS/Scripts/UnitMover.cs
+++ Money_Tree/Assets/RTS/Scripts/UnitMover.cs
@@ -8,9 +8,6 @@ public class UnitMover : MonoBehaviour
     // The offset from the target point where to move the unit to.
     public Vector3 Offset = Vector3.zero;
 
-    // The color the sprite will glow when selected
-    public Color SelectionGlowColor = Color.yellow;
-
     // Flip the sprite's X based on where we are moving towards?
     public bool FlipSpriteX = true;
 
@@ -20,45 +17,23 @@ public class UnitMover : MonoBehaviour
 
 
     uint _flipCooldown; // How many frames have to pass 'til the next sprite flip
-    bool _selected = false;
-    MaterialPropertyBlock _materialProps; // (Used to control the glow when selected)
     SpriteRenderer _spriteRenderer;
     NavMeshAgent _navAgent;
+    Selectable _selectable;
+
 
 	void Start()
 	{
         _flipCooldown = 0;
-        _spriteRenderer = GetComponent<SpriteRenderer>();
         _navAgent = GetComponent<NavMeshAgent>();
-
-
-        // Initialize defaults
-        _materialProps = new MaterialPropertyBlock();
-        _selected = false;
-        ApplyGlowColor(Color.black); // Initially deselected = no glow
+        _selectable = GetComponent<Selectable>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
 	}
-
-
-    // Applies the given color to "Sprites/Edge"'s _EdgeGlowColor
-    void ApplyGlowColor(Color color)
-    {
-        _spriteRenderer.GetPropertyBlock(_materialProps);
-        _materialProps.SetColor("_EdgeGlowColor", color);
-        _spriteRenderer.SetPropertyBlock(_materialProps);
-    }
-
-	void OnMouseDown()
-	{
-        // Clicking on an unit's collider [de]selects it
-        _selected = !_selected;
-        ApplyGlowColor(_selected ? SelectionGlowColor : Color.black);
-	}
-
 
     // Tell the unit to start moving towards a point if it was selected
     public void UnitMoveTowards(Vector3 point)
     {
-        if(_selected)
+        if(_selectable.Selected)
         {
             //Debug.Log("Unit " + this + " move towards " + point);
             _navAgent.SetDestination(point + Offset);
