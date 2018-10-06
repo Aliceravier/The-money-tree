9ab398039b309391c61f3e1119ca1b0cc6aacbf6
Paolo Jovon <paolo.jovon@gmail.com>
2018-10-06 09:41:24 +0100

    Added a Billboard script for sprites and stubbed a Spawner script for later use
    + added a test sprite to the game

---
+++ Money_Tree/Assets/RTS/Scripts/Billboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    // The camera to face towards.
+    public Camera Camera = null;
+
+    Quaternion _baseRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        _baseRotation = this.transform.rotation;
+    }
+    
+    // Update is called once per frame when `Update()`s are done
+    void LateUpdate()
+    {
+        this.transform.rotation = _baseRotation
+                                  * Camera.transform.rotation;
+    }
+}
---
+++ Money_Tree/Assets/RTS/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	// The interval in seconds between consecutive spawns
+	public float Interval = 10.0f;
+
+	// The prefab to spawn
+	public GameObject Prefab = null;
+
+	// The radius around this.Transform.origin where to spawn Prefabs
+	public float Radius = 1.0f;
+
+
+	// Use this for initialization
+	void Start()
+    {
+        this.InvokeRepeating("Spawn", Interval, Interval);
+	}
+
+	// Called when gizmos are to be drawn
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(this.transform.position, Radius);
+    }
+
+	
+	// Spawns an enemy
+	public void Spawn()
+    {
+        // FIXME IMPLEMENT Object.Instantiate(Prefab, );
+	}
+}
