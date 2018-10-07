5e2f86d5176122f81912559145670e17affc9d1a
Aetherflare <thekeltonator@gmail.com>
2018-10-07 17:07:32 +0100

    This Version Works
    It does

---
+++ Money_Tree/Assets/Winner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winner : MonoBehaviour {
+    int timer;
+    Sprite[] screenSprites;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
