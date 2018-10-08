c6b82076e050a8bf77aca7da6902ec7bc9070a1d
Paolo Jovon <paolo.jovon@gmail.com>
2018-10-08 17:14:20 +0100

    Fixed camera bounds in PartOne

---
+++ Money_Tree/Assets/CameraPreview.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPreview : MonoBehaviour
+{
+    // Use this for initialization
+    public CameraPreview()
+    {
+    }
+
+    // Always draw gizmos
+    void OnDrawGizmos()
+    {
+        Camera orthoCam = this.gameObject.GetComponent<Camera>();
+        var center = this.transform.position;
+        var bounds = new Vector3(orthoCam.orthographicSize * orthoCam.aspect * 2.0f,
+            orthoCam.orthographicSize * 2.0f,
+            1.0f);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(center, bounds);
+    }
+}
