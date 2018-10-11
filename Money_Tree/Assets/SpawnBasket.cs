40e291c7f57e15d9eef75a44b5be9a2fbb7e5631
Aetherflare <thekeltonator@gmail.com>
2018-10-11 20:35:30 +0100

    Baskets semi-functional

--- Money_Tree/Assets/SpawnBasket.cs
+++ Money_Tree/Assets/SpawnBasket.cs
@@ -8,31 +8,57 @@ public class SpawnBasket : MonoBehaviour
     private GameObject basketInstance;
     public int spawnX;
     public int spawnY;
+    private bool placed;
+    public Camera cam = null;
     // Use this for initialization
     void Start()
     {
 
         spawnX = 375;
         spawnY = 30;
+        placed = false;
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If this is not a clone, check if we bought a new sprinkler
+        //If this is not a clone, check if we bought a new basket
         if (!gameObject.name.Contains("Clone") && BuyBasket.basket)
         {
+            //Put the basket at the mouse pointer but have it follow pointer until placed
             basketInstance = Instantiate(Resources.Load("Basket"),
-            new Vector3(spawnX, spawnY),
+            cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y)),
             Quaternion.identity) as GameObject;
-            spawnX -= 15;
+            this.placed = false;
+            /**spawnX -= 15;
             if (spawnX < 200)
             {
                 spawnX = 650;
                 spawnY += 5;
-            }
+            }*/
             BuyBasket.basket = false;
             this.name = "basket";
+        }
+        if  (!placed && !gameObject.name.Contains("basketball"))
+        {
+            gameObject.transform.SetPositionAndRotation(cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y + 20)), Quaternion.identity);
+        }
+    }
+
+    void onMouseDown()
+    {
+        if (!placed)
+        {
+            placed = true; 
+            if (!gameObject.name.Contains("basketball"))
+            {
+                Debug.Log("placed is " + placed);
+            }
+            
         }
     }
 }
